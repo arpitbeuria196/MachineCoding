31e80f6a6d762c1981f18cc1365937ba95569c8b
arpitbeuria196 <arpitbeuria196@gmail.com>
2025-04-16 14:09:55 +0100

    ToDo Application

--- ToDOApp/src/App.jsx
+++ ToDOApp/src/App.jsx
@@ -1,34 +1,83 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks,setTasks] = useState([]);
+  const[taskName,setTaskName] = useState("");
+  const [editToggle,setEditToogle] = useState(false);
+  const [editIndex,setEditIndex] = useState();
+
+  const handleAddTask = ()=>
+  {
+    if(taskName.trim()=="") return;
+
+    const copyArray = [...tasks];
+      if(editToggle)
+      {
+        copyArray[editIndex] = taskName;
+        setTasks(copyArray);
+        setEditIndex(null);
+        setEditToogle(false);
+        console.log(copyArray);
+      }
+      else
+      {
+      copyArray.push(taskName);
+      setTasks(copyArray);
+      console.log(copyArray);
+      }
+      setTaskName("");
+
+  }
+
+  const handleDeleteTask = (index)=>
+  {
+    const copyArray = [...tasks];
+   const filtered = copyArray.filter((task,ind)=> ind!=index)
+    setTasks(filtered);
+  }
+
+  const handleEditTask = (index)=>
+  {
+    setEditToogle(true);
+    setTaskName(tasks[index]);
+    setEditIndex(index);
+
+  }
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='min-h-screen min-w-screen flex flex-col items-center justify-center text-center bg-gray-500'>
+        <input
+        placeholder='Enter Task Name'
+        className='m-5 p-5 border rounded-2xl'
+        value={taskName}
+        onChange={(e)=>setTaskName(e.target.value)}
+        
+        />
+        <button
+        onClick={handleAddTask}
+        className='mb-2'
+        >{editToggle ? "Update Task" : "Add Task"}</button>
+        {
+          tasks.map((task,index)=>(
+              <div 
+              className='flex items-center justify-between border p-3 mb-3 rounded gap-2 '
+              key={index}>
+                {task}
+                <button
+                onClick={()=>handleDeleteTask(index)}
+                >delete</button>
+                <button
+                onClick={()=>handleEditTask(index)}
+                >
+                  Edit
+                </button>
+              </div>
+          ))
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
